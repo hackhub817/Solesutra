b34440ea005b64db8745538883ecf2529110ac81
hackhub817 <piyushguptaji123@gmail.com>
2025-10-07 22:41:45 +0530

    changes done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
           {/* Logo Image */}
           <div className="flex items-center gap-3">
             <img
-              src={logo} // 👉 replace with your logo file path
+              src={logo} // 👉 h
               alt="SoleSutra Logo"
               className="lg:h-8 lg:w-8 h-8 w-8 sm:h-10 sm:w-10 object-contain"
             />
